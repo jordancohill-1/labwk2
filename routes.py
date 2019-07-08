14f2b03d5bc4f0eb5d51f35d7a4029d4d88fdfdc
jordancohill-1 <cohill_jordan@bah.com>
2019-07-08 11:01:54 -0600

    four

--- routes.py
+++ routes.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+	model = joblib.load('regressor.pkl')
 	prediction = model.predict([[4, 2.5, 3005, 15, 17903.0]]).round(2)
 	prediction = np.squeeze(prediction)
 	return render_template("index.html", prediction=prediction)
 
 if __name__ == '__main__':
-	model = joblib.load('regressor.pkl')
 	app.run(debug=True)
 
 	
